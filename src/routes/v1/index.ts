df62b6b695eca18f12c8f6c40bd4676b5426da30
Michał Jadach <michal.jadach@hoprnet.org>
2025-12-07 15:05:30 +0100

    cleanup repo

--- src/routes/v1/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import airdropRoutes from './airdropRoutes';
-
-const router = Router();
-
-// Mount v1 routes
-router.use('/airdrop', airdropRoutes);
-
-export default router;
